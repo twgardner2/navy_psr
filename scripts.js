0181f5cb20f3a610a9c33fb601f8162ae1d176e5
Tom Gardner <twgardner2@gmail.com>
2021-03-25 06:45:07 -0500

    initial commit

--- scripts.js
+++ scripts.js
@@ -2,50 +2,82 @@
 
 import * as lib from './lib/lib.js';
 
-console.log(lib.test_var);
 document.addEventListener('DOMContentLoaded', function() {
 
-    // Create scales
+    // Horizontal (time) scale
     const time_scale = d3.scaleTime()
-                    .domain([Date.now() - 10*365*24*60*60*1000, Date.now()])
+                    .domain([Date.now() - 15*365*24*60*60*1000, Date.now()])
                     .range([0, lib.fitrep_width]);
 
-
+    // Vertical (RSCA) scale
     const rsca_scale = d3.scaleLinear()
                     .domain([-1,1])
                     .range([0, lib.fitrep_height]);
 
-
+    // FITREP canvas
     const fitrep_canvas = d3.select('#fitreps')
                         .append('svg')
                         .attr('width', lib.fitrep_width + lib.margin.left + lib.margin.right)
-                        .attr('height', lib.fitrep_height + lib.margin.bottom + lib.margin.top);
+                        .attr('height', lib.fitrep_height + lib.margin.bottom + lib.margin.top)
+                        .append('g')
+                        .attr('id', 'fitrep_canvas')
+                        .attr('transform', `translate(${lib.margin.left}, ${lib.margin.top})`);
 
+    // Rank bar canvas
+    const rank_canvas = d3.select('#rank')
+                            .append('svg')
+                            .attr('width', lib.bar_width)
+                            .attr('height', lib.bar_height)
+                            .append('g')
+                            .attr('id', 'fitrep_canvas')
+                            .attr('transform', `translate(${lib.margin.left}, ${lib.margin.top})`);
+    // Command bar canvas
+    // Rank bar canvas
+    const command_canvas = d3.select('#command')
+                            .append('svg')
+                            .attr('width', lib.bar_width)
+                            .attr('height', lib.bar_height)
+                            .append('g')
+                            .attr('id', 'fitrep_canvas')
+                            .attr('transform', `translate(${lib.margin.left}, ${lib.margin.top})`);
+
+    // Reporting Senior bar canvas
+    const rep_sen_canvas = d3.select('#rep_sen')
+                            .append('svg')
+                            .attr('width', lib.bar_width)
+                            .attr('height', lib.bar_height)
+                            .append('g')
+                            .attr('id', 'fitrep_canvas')
+                            .attr('transform', `translate(${lib.margin.left}, ${lib.margin.top})`);
     
     const draw_fitreps = data => {
 
+        // Extract member's name and update H1
         const member_name = data[0].name;
-        d3.select('h1')
-            .text(`PSR - ${member_name}`);
+        if (member_name) d3.select('h1').text(`PSR - ${member_name}`);
         
         // Draw time axis
         fitrep_canvas.append('g')
                     .attr('class', 'x axis')
-                    .attr('transform', 'translate(0,' + (lib.fitrep_height-rsca_scale(0)) + ')')
+                    .attr('transform', `translate(0, ${lib.fitrep_height-rsca_scale(0)})`)
                     .call(d3.axisBottom(time_scale)); // Create an axis component with d3.axisBottom
 
         // Draw "Trait Avg - RSCA" axis
+        fitrep_canvas.append('g')
+                    .attr('class', 'y axis')
+                    .call(d3.axisLeft(rsca_scale)); // Create an axis component with d3.axisBottom
 
-        fitrep_canvas.selectAll('g').data(data)
+        // Draw FITREPs
+        fitrep_canvas.selectAll('g.fitrep')
+                        .data(data)
                         .enter()
                         .append('g')
                         .attr('transform', function(d) {
-                            return 'translate(' + time_scale(d.end_date) + ', ' + (lib.fitrep_height-rsca_scale(d.trait_avg-d.rsca)) + ')'
+                            return `translate(${time_scale(d.end_date)}, ${lib.fitrep_height-rsca_scale(d.trait_avg-d.rsca)})`
                         })
                         .append('text')
                         .text(d => {
                             var val = d.prom_rec.toLowerCase();
-                            // console.log(val.toUpperCase());
                             val = val==="nob" ? "n" : val;
                             return (val.toUpperCase());
                         });
