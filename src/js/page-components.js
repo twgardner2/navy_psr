2ac48c8d9b5601a8bde77b2c7160e55638f2151b
Tom Gardner <twgardner2@gmail.com>
2022-01-14 21:07:12 -0500

    add SELRES bars, update colors

--- src/js/page-components.js
+++ src/js/page-components.js
@@ -1,130 +1,148 @@
-import * as lib from "./lib.js";
-import * as d3 from "d3";
+import * as lib from './lib.js';
+import * as d3 from 'd3';
 
 let svg,
     container_g,
     rank_g,
-    command_g,
-    rep_sen_g,
-    command_cc_g,
-    rep_sen_cc_g,
+    command_rg_g,
+    rep_sen_rg_g,
+    command_idt_g,
+    rep_sen_idt_g,
+    command_at_g,
+    rep_sen_at_g,
     fitreps_g,
     legend_canvas,
     table,
     rerender_button,
     table_container;
 
-
-export const getPageElements= ()=>({
+export const getPageElements = () => ({
     rank_g,
-    command_g,
-    rep_sen_g,
-    command_cc_g,
-    rep_sen_cc_g,
+    command_rg_g,
+    rep_sen_rg_g,
+    command_idt_g,
+    rep_sen_idt_g,
+    command_at_g,
+    rep_sen_at_g,
     fitreps_g,
     legend_canvas,
     rerender_button,
 });
 
-export const buildElements= (grid)=>{  
-
+export const buildElements = (grid) => {
     buildGraph(grid);
 
     // Legend SVG canvas
     legend_canvas = grid
-    .append("div")
-    .attr("id", "fitreps_legend")
-    .append("svg")
-    .attr("width", "100%");
+        .append('div')
+        .attr('id', 'fitreps_legend')
+        .append('svg')
+        .attr('width', '100%');
 
-    
-    table_container = grid
-    .append("div")
-    .attr("class", "table");
+    table_container = grid.append('div').attr('class', 'table');
 
-    
     rerender_button = table_container
-    .append("div")
-    .append("button")
-    .text("Re-Render")
+        .append('div')
+        .append('button')
+        .text('Re-Render');
 
-    return getPageElements()
-}
+    return getPageElements();
+};
 
-
-export function resetGraph(grid){
+export function resetGraph(grid) {
     svg.remove();
     buildGraph(grid);
 }
 
-function buildGraph(grid){
+function buildGraph(grid) {
     svg = grid
-    .append("svg")
-    .attr("id", "canvas")
-    .attr("width", lib.canvas_width)
-    .attr("height", lib.canvas_height);
+        .append('svg')
+        .attr('id', 'canvas')
+        .attr('width', lib.canvas_width)
+        .attr('height', lib.canvas_height);
 
-    container_g = svg
-    .append("g")
-    .attr("id", "container_g");
+    container_g = svg.append('g').attr('id', 'container_g');
 
     rank_g = container_g
-    .append("g")
-    .attr("id", "rank_g")
-    .attr("transform", `translate(${lib.y_axis_width}, ${lib.margin.gap})`);
+        .append('g')
+        .attr('id', 'rank_g')
+        .attr('transform', `translate(${lib.y_axis_width}, ${lib.margin.gap})`);
 
-    command_g = container_g
-    .append("g")
-    .attr("id", "command_g")
-    .attr("class", "bar_container")
-    .attr(
-    "transform",
-    `translate(${lib.y_axis_width}, ${lib.bar_height + lib.margin.gap})`
-    );
-
-    rep_sen_g = container_g
-    .append("g")
-    .attr("id", "rep_sen_g ")
-    .attr(
-    "transform",
-    `translate(${lib.y_axis_width}, ${2 * lib.bar_height + lib.margin.gap})`
-    );
-
-    command_cc_g = container_g
-    .append("g")
-    .attr("id", "command_cc_g")
-    .attr(
-    "transform",
-    `translate(${lib.y_axis_width}, ${
-        3 * lib.bar_height + 3 * lib.margin.gap
-    })`
-    );
-
-    rep_sen_cc_g = container_g
-    .append("g")
-    .attr("id", "rep_sen_cc_g ")
-    .attr(
-    "transform",
-    `translate(${lib.y_axis_width}, ${
-        4 * lib.bar_height + 3 * lib.margin.gap
-    })`
-    );
-
+    // Regular (Active Duty) Commands
+    command_rg_g = container_g
+        .append('g')
+        .attr('id', 'command_rg_g')
+        .attr('class', 'bar_container')
+        .attr(
+            'transform',
+            `translate(${lib.y_axis_width}, ${lib.bar_height + lib.margin.gap})`
+        );
+    // Regular (Active Duty) Reporting Seniors
+    rep_sen_rg_g = container_g
+        .append('g')
+        .attr('id', 'rep_sen_rg_g')
+        .attr(
+            'transform',
+            `translate(${lib.y_axis_width}, ${
+                2 * lib.bar_height + lib.margin.gap
+            })`
+        );
+    // IDT (SELRES) Commands
+    command_idt_g = container_g
+        .append('g')
+        .attr('id', 'command_idt_g')
+        .attr('class', 'bar_container')
+        .attr(
+            'transform',
+            `translate(${lib.y_axis_width}, ${
+                3 * lib.bar_height + lib.margin.gap
+            })`
+        );
+    // IDT (SELRES) Reporting Seniors
+    rep_sen_idt_g = container_g
+        .append('g')
+        .attr('id', 'rep_sen_idt_g')
+        .attr(
+            'transform',
+            `translate(${lib.y_axis_width}, ${
+                4 * lib.bar_height + lib.margin.gap
+            })`
+        );
+    // AT (Mobilization/ADSW/ADT) Commands
+    command_at_g = container_g
+        .append('g')
+        .attr('id', 'command_at_g')
+        .attr(
+            'transform',
+            `translate(${lib.y_axis_width}, ${
+                5 * lib.bar_height + lib.margin.gap
+            })`
+        );
+    // AT (Mobilization/ADSW/ADT) Reporting Seniors
+    rep_sen_at_g = container_g
+        .append('g')
+        .attr('id', 'rep_sen_at_g')
+        .attr(
+            'transform',
+            `translate(${lib.y_axis_width}, ${
+                6 * lib.bar_height + lib.margin.gap
+            })`
+        );
     fitreps_g = container_g
-    .append("g")
-    .attr("id", "fitreps_g")
-    .attr(
-    "transform",
-    `translate(${lib.y_axis_width}, ${
-        6 * lib.bar_height + 5 * lib.margin.gap
-    })`
-    );
+        .append('g')
+        .attr('id', 'fitreps_g')
+        .attr(
+            'transform',
+            `translate(${lib.y_axis_width}, ${
+                6 * lib.bar_height + 5 * lib.margin.gap
+            })`
+        );
 }
 
-export function resetTable(){
-    if(table){
+export function resetTable() {
+    if (table) {
         table.remove();
     }
-    table = table_container.append("table").attr("id", "fitrep_table");
+    table = table_container.append('table').attr('id', 'fitrep_table');
     return table;
 }
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,57 +1,50 @@
-
-import * as lib from "./lib.js";
-import * as d3 from "d3";
+import * as lib from './lib.js';
+import * as d3 from 'd3';
 
 const { DataProvider } = require('./data/providers/DataProvider');
-const {parse_data_from_table}=require('./data/parsers/table/table-parser');
+const { parse_data_from_table } = require('./data/parsers/table/table-parser');
 
-const { buildElements } = require("./page-components.js");
-const {populate_table} =require("./table/table.js");
-const {clear_psr_viz, draw_psr_viz}= require("./graph/graph.js");
+const { buildElements } = require('./page-components.js');
+const { populate_table } = require('./table/table.js');
+const { clear_psr_viz, draw_psr_viz } = require('./graph/graph.js');
 
-const {appendPDFUploadForm}= require("./pdf-form.js");
+const { appendPDFUploadForm } = require('./pdf-form.js');
 
 let data;
 
-document.addEventListener("DOMContentLoaded", function () {
-let grid = d3.select("body").select(".grid");
+document.addEventListener('DOMContentLoaded', function () {
+    let grid = d3.select('body').select('.grid');
 
-  appendPDFUploadForm(grid);
+    appendPDFUploadForm(grid);
 
-  const {
-    rerender_button
-  }=buildElements(grid);
+    const { rerender_button } = buildElements(grid);
 
- rerender_button.on("click", function (event) {
-  var table_data = parse_data_from_table();
-  clear_psr_viz(document.getElementById("canvas"));
-  let provider=new DataProvider(table_data);
-  draw_psr_viz(provider);
+    rerender_button.on('click', function (event) {
+        var table_data = parse_data_from_table();
+        clear_psr_viz(document.getElementById('canvas'));
+        let provider = new DataProvider(table_data);
+        draw_psr_viz(provider);
+    });
 
+    let data = d3
+        .csv('./gardner_anon.csv', d3.autoType)
+        .then((data) => {
+            // Attach original data to ghost <g> to retain it
+            d3.select('body')
+                .append('g')
+                .attr('id', 'original_data')
+                .attr('original_data', data)
+                .data(data);
 
-});
-
-let data = d3
-    .csv("./gardner_anon.csv", d3.autoType)
-    .then((data) => {
-      // Attach original data to ghost <g> to retain it
-      d3.select("body")
-        .append("g")
-        .attr("id", "original_data")
-        .attr("original_data", data)
-        .data(data);
-
-      // Log data to console
-      // console.log(data);
-      return data;
-    })
-    .then((data) => {
-      let provider= new DataProvider(data)
-      populate_table(provider);
-      return provider;
-    })
-    // Draw viz
-    .then((provider) => draw_psr_viz( provider ));
-
-
+            // Log data to console
+            // console.log(data);
+            return data;
+        })
+        .then((data) => {
+            let provider = new DataProvider(data);
+            populate_table(provider);
+            return provider;
+        })
+        // Draw viz
+        .then((provider) => draw_psr_viz(provider));
 });
